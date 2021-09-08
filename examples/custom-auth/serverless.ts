b5c41722c6aa8f41cee1baf8171b863b93f6ef1c
Ben Watts <ben@kilowatts.io>
2021-09-08 10:54:06 +0100

    add custom authorizer

---
+++ examples/custom-auth/serverless.ts
@@ -0,0 +1,41 @@
+// Requiring @types/serverless in your project package.json
+import type { Serverless } from 'serverless/aws';
+
+const serverlessConfiguration: Serverless = {
+    frameworkVersion: '2',
+    provider: {
+        name: 'aws',
+        stage: 'dev',
+        runtime: 'nodejs14.x',
+    },
+    service: 'users',
+    functions: {
+        usersCreate: {
+            handler: 'src/handler.hello',
+            events: [
+                {
+                    http: {
+                        path: '/hello',
+                        method: 'get',
+                        authorizer: {
+                            name: 'authorizer',
+                            type: 'token',
+                            resultTtlInSeconds: 0,
+                            identitySource: 'method.request.header.Authorization'
+                        }
+                    }
+                },
+            ],
+        },
+        authorizer: {
+            handler: 'src/auth.handler',
+
+        }
+    },
+    plugins: [
+        'serverless-plugin-typescript',
+        'serverless-offline'
+    ]
+};
+
+module.exports = serverlessConfiguration;
